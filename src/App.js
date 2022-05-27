552723eb7fb16e7231672155c79e8e756e7157c6
Ronald Benitez <ronald05101998@gmail.com>
2022-05-26 22:01:35 -0500

    cambio de api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect,useState } from 'react';
 
 function App() {
-  const url = ' https://dog.ceo/api/breeds/list/all';
-  const [data, setData] = useState([]);
+  const url = 'https://jsonplaceholder.typicode.com/todos';
+  const [data, setData] = useState();
   const fechtApi =async () => {
     const response = await fetch(url);
-    const dataJson = await response.json();
-    setData(dataJson.message);
-    console.log(data);
     
-  }  
+    const dataJson = await response.json();
+    setData(dataJson);
+    //console.log(data);
+  }
   useEffect(() => {
     fechtApi();
   }, [])
   return (
 
-    <div className="App">
-      hola mundo
+    <div className="App"> 
+      {
+        data?data.map(item => {
+          return (
+            <div key={item.id}>
+              <h3>{item.title}</h3>
+              <p>{item.completed}</p>
+            </div>
+          )
+        }):"Loading..."
+      }
     </div>
   );
 }
